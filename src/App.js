ff5e10fa9aa5de162935c0506afb7ab0ef5f4356
Dastinss <sergeyfdaragan@gmail.com>
2023-02-23 01:20:47 +0200

    #16 les done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 // import {Accordion} from "./components/Accordion/Accordion";
 // import {Rating} from "./Rating/Rating";
-import {OnOff} from "./components/OnOff/OnOff";
-import {UncontrolledAccordion} from "./components/UncontrolledAccordion/UncontrolledAccordion";
-import {UnconrolledRating} from "./UncontrolledRating/UncontrolledRating";
-import {useState} from "react";
+import { OnOff } from "./components/OnOff/OnOff";
+import { UncontrolledAccordion } from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import { UnconrolledRating } from "./UncontrolledRating/UncontrolledRating";
+import { useState } from "react";
 // import type {RatingValueType} from "./Rating/Rating";
-import {UncontrolledOnOff} from "./components/UncontrolledOnOff/UncontrolledOnOff";
+import { UncontrolledOnOff } from "./components/UncontrolledOnOff/UncontrolledOnOff";
+import { ReactMemo } from "./components/ReactMemo";
 // import {Select} from "./components/Select/Select";
 
 // function hello() {
@@ -15,53 +16,52 @@ import {UncontrolledOnOff} from "./components/UncontrolledOnOff/UncontrolledOnOf
 
 // hello();
 
-function App(props: any) {
-  console.log('App rendering')
+function App( props: any ) {
+    console.log( 'App rendering' )
 
-  // let [ratingValue, setRatingValue] = useState(4);
-  let [accordionCollapsed, setAccordionCollapsed] = useState(false)
-  let [switchOn, setSwitchOn]= useState(false);
+    // let [ratingValue, setRatingValue] = useState(4);
+    let [accordionCollapsed, setAccordionCollapsed] = useState( false )
+    let [switchOn, setSwitchOn] = useState( false );
 
-  return (
-      <div className={'App'}>
-        {/*<Select />*/}
-        {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
+    return (
+        <div className={'App'}>
+            {/*<Select />*/}
+            {/*<Rating value={ratingValue} onClick={setRatingValue}/>*/}
 
-        <OnOff on = {switchOn} onChange={setSwitchOn}/>
-        {/*<OnOff/>*/}
-        {/*<OnOff/>*/}
-        {/*<OnOff/>*/}
-        {/*<PageTitle title={'This is App component'}/>*/}
-        {/*<PageTitle title={'My friends'}/>*/}
-        {/*Article 1*/}
-        {/*<Accordion titleValue = {'Menu'} collapsed = {accordionCollapsed}*/}
-        {/*           onChange={() => {setAccordionCollapsed(!accordionCollapsed)}}/>*/}
-        {/*<Accordion titleValue = {'User'} collapsed={false}/>*/}
-        <UncontrolledAccordion titleValue = {'Menu'}/>
-        <UncontrolledAccordion titleValue = {'User'}/>
-        {/*<Rating value={0}/>*/}
-        {/*<Rating value={1}/>*/}
-        {/*<Rating value={2}/>*/}
-        {/*<Rating value={3}/>*/}
-        {/*<Rating value={4}/>*/}
-        {/*<Rating value={5}/>*/}
-        <UnconrolledRating />
-        {/*<UncontrolledOnOff/ /// ниже улучшение, где компонента нас будет уведомлять true/false />*/}
-        <UncontrolledOnOff onChange = {setSwitchOn}/> {switchOn.toString()}
-
-      </div>
-  );
+            <OnOff on={switchOn} onChange={setSwitchOn}/>
+            {/*<OnOff/>*/}
+            {/*<OnOff/>*/}
+            {/*<OnOff/>*/}
+            {/*<PageTitle title={'This is App component'}/>*/}
+            {/*<PageTitle title={'My friends'}/>*/}
+            {/*Article 1*/}
+            {/*<Accordion titleValue = {'Menu'} collapsed = {accordionCollapsed}*/}
+            {/*           onChange={() => {setAccordionCollapsed(!accordionCollapsed)}}/>*/}
+            {/*<Accordion titleValue = {'User'} collapsed={false}/>*/}
+            <UncontrolledAccordion titleValue={'Menu'}/>
+            <UncontrolledAccordion titleValue={'User'}/>
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+            <UnconrolledRating/>
+            {/*<UncontrolledOnOff/ /// ниже улучшение, где компонента нас будет уведомлять true/false />*/}
+            <UncontrolledOnOff onChange={setSwitchOn}/> {switchOn.toString()}
+            <ReactMemo/>
+        </div>
+    );
 }
 
 type PageTitlePropsType = {
-  title: string
+    title: string
 }
 
-function PageTitle(props: PageTitlePropsType) {
-  console.log('PageTitle rendering')
-  // return <h1>This is App component</h1> // так два раза прпищется один и тот же текст, который тут прописан, несмотря на то, что в татл во втором вызове стоит "'My friends'", к примеру
-  return <h1>{props.title}</h1>
+function PageTitle( props: PageTitlePropsType ) {
+    console.log( 'PageTitle rendering' )
+    // return <h1>This is App component</h1> // так два раза прпищется один и тот же текст, который тут прописан, несмотря на то, что в татл во втором вызове стоит "'My friends'", к примеру
+    return <h1>{props.title}</h1>
 }
-
 
 export default App;
---
+++ src/components/ReactMemo.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const NewMessagesCounter = ( props: { count: number } ) => {
+    return <div>{props.count}</div>
+}
+
+const UsersSecret = ( props: { users: Array<string> } ) => {
+    console.log('USERS')
+    return <div>
+        {props.users.map( ( u, i ) => <div key={i}>{u}</div> )}
+    </div>
+}
+
+const Users = React.memo (UsersSecret); //єто контейнерная компонента которая смотит не делается ли лищних перересовок и потом их не перерисовывает если не было изменений пропсов\данных
+
+export const ReactMemo = () => {
+    const [counter, setCounter] = useState( 0 )
+    const [user, setUser] = useState( ['Serg', 'Ann', 'Mike'] )
+
+    const addUser = () => {
+        const newUsers = [...user, 'Sveta' + new Date().getDate()]
+        setUser(newUsers);
+    }
+
+    return <>
+        {/*<NewMessagesCounter count={10}/>*/}
+        {/*<Users users={['Serg', 'Ann', 'Mike']}/>*/}
+        <button onClick={() => {setCounter(counter+1)}}> + </button>
+        <button onClick={addUser}> add user </button>
+        <NewMessagesCounter count={counter}/>
+        <Users users={user}/>
+    </>
+}
